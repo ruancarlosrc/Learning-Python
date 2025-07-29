445d995197037b29e7cc9146e9d9bc32125f0cbc
Ruan Carlos <ruancarlos.rc794@gmail.com>
2025-07-28 22:58:14 -0300

    fist structures

---
+++ structures.py
@@ -0,0 +1,17 @@
+"""
+print("*" * 20)
+print("Hello ;)")
+###
+print("Which number is bigger")
+print("*" * 20)
+print("Please type two numbers:")
+num1 = int(input("First number: "))
+num2 = int(input("Second number: "))
+if num1 > num2:
+    print(" The first number is the biggest.")
+elif num2 > num1:
+    print(" The second number is the biggest.")
+else:
+    print(" The numbers are the same! ")
+###
+"""
