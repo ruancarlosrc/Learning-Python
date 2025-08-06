50b96e3bd52c40a4e45171c106b8f809fbc3e3de
Ruan Carlos <ruancarlos.rc794@gmail.com>
2025-08-05 23:24:33 -0300

    lazyness

--- structures2.py
+++ structures2.py
@@ -1,3 +1,4 @@
+"""
 print("Odd or even!")
 print("Please, type a number: ")
 num = int(input())
@@ -6,3 +7,16 @@ if result == 1:
     print("ODD!")
 elif result == 0:
     print("Even!")
+###
+"""
+print("type two numbers:")
+num1 = int(input("Type the first number: "))
+num2 = int(input("Type the seconde number: "))
+if num1 > num2:
+    print("The first number is bigger")
+elif num2 > num1:
+    print("The seconde number is bigger")
+elif num1 == num2:
+    print("The numbers are the same!")
+dif = num1 - num2
+print(f"The difference between the numbers are {dif}")
